1667aa4981844de36c26754cb0b1800b29c0b03a
Jissmon Jose <jissmon476@gmail.com>
2023-11-21 15:21:26 +0530

    Refactor getUserDetails method and add error handling

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 class GitHub {
+    constructor() {
+      this.main = document.getElementById('main');
+    }
+    
+    showCard() {
+        this.main.style.display = 'block';
+    }
+
     async getUserDetails(username) {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      const data = await response.json();
-  
-      this.createUserCard(data);
+        this.main.style.display = 'none'; 
+        try {
+            const response = await fetch(`https://api.github.com/users/${username}`);
+            if (!response.ok) {
+                throw new Error(response.status === 404 ? 'User not found' : 'Error fetching data');
+            }
+            const data = await response.json();
+            this.createUserCard(data);
+            this.showCard();
+        } catch (error) {
+            this.showError(error.message);
+        }
     }
   
     createUserCard(user) {
@@ -14,32 +30,34 @@ class GitHub {
           </div>
           <div>
             <h2>${user.name}</h2>
-            <p>${user.bio}</p>
+            <p>${user.bio ? user.bio : 'No bio available'}</p>
             <ul>
               <li>${user.followers} Followers</li>
               <li>${user.following} Following</li>
               <li>${user.public_repos} Repos</li>
             </ul>
-            <div>${user.location || ''}</div>
-            <div>${user.twitter_username || ''}</div>
+            <div>${user.location ? `Location: ${user.location}` : ''}</div>
+            <div>${user.twitter_username ? `Twitter: @${user.twitter_username}` : ''}</div>
           </div>
         </div>
       `;
+      this.main.innerHTML = cardHTML;
+    }
   
-      const main = document.getElementById('main');
-      main.innerHTML = cardHTML;
+    showError(message) {
+      const errorHTML = `<p>${message}</p>`;
+      this.main.innerHTML = errorHTML;
     }
   }
   
   const form = document.getElementById('user-form');
   const search = document.getElementById('search');
-  
   const gitHub = new GitHub();
   
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    gitHub.getUserDetails(search.value);
+    gitHub.getUserDetails(search.value.trim());
   });
   
-  // Initialize with your own GitHub profile
-  gitHub.getUserDetails('your-username');
+  // Don't display the card initially
+  gitHub.main.style.display = 'none';
